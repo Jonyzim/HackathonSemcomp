e64b1d9e59ac585aade8a802d1265f9deb6ed901
Jonyzim <35571614+Jonyzim@users.noreply.github.com>
2023-10-21 16:39:39 -0300

    teste article-recomender

--- app.py
+++ app.py
@@ -1,61 +1,88 @@
-import cohere
-api_key="L41u8TnPpclKHjF0jJCxjD0SZ8O5yFQOaXoTibRL"
-co = cohere.Client(api_key)
-text ="""Tão natural quanto a luz do dia
-Mas que preguiça boa, me deixa aqui à toa
-Hoje ninguém vai estragar meu dia
-Só vou gastar energia pra beijar sua boca
+# Copyright (c) 2023 Cohere Inc. and its affiliates.
+#
+# Licensed under the MIT License (the "License");
+# you may not use this file except in compliance with the License.
+#
+# You may obtain a copy of the License in the LICENSE file at the top
+# level of this repository.
 
-Fica comigo então, não me abandona, não
-Alguém te perguntou como é que foi seu dia?
-Uma palavra amiga, uma notícia boa
-Isso faz falta no dia a dia
-A gente nunca sabe quem são essas pessoas
+import numpy as np
+import pandas as pd
+import recommender as rec
+import streamlit as st
 
-Eu só queria te lembrar
-Que aquele tempo eu não podia fazer mais por nós
-Eu estava errado e você não tem que me perdoar
-Mas também quero te mostrar
-Que existe um lado bom nessa história
-Tudo que ainda temos a compartilhar
+st.set_page_config(layout="wide")
 
-E viver e cantar
-Não importa qual seja o dia
-Vamos viver, vadiar
-O que importa é nossa alegria
-Vamos viver e cantar
-Não importa qual seja o dia
-Vamos viver, vadiar
-O que importa é nossa alegria
 
-Tão natural quanto a luz do dia
-Mas que preguiça boa, me deixa aqui à toa
-Hoje ninguém vai estragar meu dia
-Só vou gastar energia pra beijar sua boca
+# Load the dataset to a dataframe
+@st.cache
+def load_df(csv_file):
+    return pd.read_csv(csv_file, delimiter=",")
 
-Eu só queria te lembrar
-Que aquele tempo eu não podia fazer mais por nós
-Eu estava errado e você não tem que me perdoar
-Mas também quero te mostrar
-Que existe um lado bom nessa história
-Tudo que ainda temos a compartilhar
 
-E viver e cantar
-Não importa qual seja o dia
-Vamos viver, vadiar
-O que importa é nossa alegria
-Vamos viver e cantar
-Não importa qual seja o dia
-Vamos viver, vadiar
-O que importa é nossa alegria
+df = load_df("bbc_news_test.csv")
+articles = df["news"].tolist()
+titles = df["title"].tolist()
 
-Tão natural quanto a luz do dia"""
-response = co.summarize(
-    text=text,
-    model='command',
-    length='medium',
-    extractiveness='medium'
-)
+# Get embeddings
 
-summary = response.summary
-print(summary)
+# Option 1 - Use this to load from pre-generated embeddings
+embs = np.load("embeddings_bbc.npy")
+
+# # Option 2 - Use this to generate new embeddings
+# embeddings = rec.embed_text(articles)
+
+# Title
+st.header("Article Recommender")
+
+# Drop down selectbox
+col1, col2 = st.columns(2)
+with col1:
+    current_id = st.selectbox("Select an Article", range(len(titles)), format_func=lambda x: titles[x])
+    st.markdown("[Source](http://mlg.ucd.ie/datasets/bbc.html)")
+
+# Get the similarity between the target and candidate articles
+current_emb = embs[current_id]
+similarity_scores = rec.get_similarity(current_emb, embs)
+
+col3, col4 = st.columns(2)
+
+# Current article
+with col3:
+    st.markdown("***You are currently reading...***")
+    current_class = rec.classify_text(articles[current_id])
+    st.markdown(f"###### {current_class.capitalize()}")
+    st.subheader(f"{titles[current_id]}")
+    current_article = articles[current_id]
+    st.write(current_article.replace("$", "\\$"))
+
+# Recommended article
+with col4:
+    st.markdown("***You might also like...***")
+    SHOW_TOP = 3
+    count = 0
+    for candidate_id, _ in similarity_scores:
+        candidate_article = articles[candidate_id]
+        candidate_title = titles[candidate_id]
+
+        candidate_class = rec.classify_text(candidate_article)
+
+        if candidate_class == current_class and candidate_id != current_id:
+            # Show title
+            st.markdown(f"###### {candidate_class.capitalize()}")
+            st.subheader(candidate_title)
+
+            # Show article
+            expander = st.expander("Read article")
+            expander.write(candidate_article.replace("$", "\\$"))
+
+            # Show tags
+            complete_prompt = rec.create_prompt(candidate_article)
+            tags = rec.extract_tags(complete_prompt)
+            st.markdown(tags)
+            st.write("------")
+
+            count += 1
+
+        if count == SHOW_TOP:
+            break
